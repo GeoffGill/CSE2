905e7cbec85440792e46e7f40e6f94700e2a6576
geoffgill <gdg216@lehigh.edu>
2014-08-28 19:00:54 +0000

    updated Welcome

---
+++ hw01-Welcome/Welcome.java
@@ -0,0 +1,28 @@
+//Geoff Gill
+//hw01
+//Welcome Java Program
+//
+//  first comple the program
+//      javac Welcome.java
+//  run the program
+//       java Welcome
+//
+//  define a class
+public class Welcome {
+    
+    //  add main method
+    public static void main(String[] args) {
+        
+//  print the welcome statment
+        System.out.println("    -----------");
+        System.out.println("    | WELCOME |");
+        System.out.println("    ___________");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-G--D--G--2--1--6-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("I am a Junior in the IBE program double majoring in Industrial Engineering and Finance. I am from Chatham NJ");
+    
+    }
+}
