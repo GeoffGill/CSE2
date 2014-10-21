84716bd1e8ae66436288c60a512ab27f16d4cca0
geoffgill <gdg216@lehigh.edu>
2014-10-21 23:24:40 +0000

    updated NumberStacks

---
+++ hw07/NumberStacks.java
@@ -0,0 +1,130 @@
+//Geoff Gill
+//hw07
+//10/22/14
+//Explanation:  This program will print out 3 pyramids of integers with four tiers in each pyramid
+        //Each pyramid is identical but they are created using for, while, and do-while loops, respectively
+
+
+import java.util.Scanner;
+
+public class NumberStacks{
+    public static void main (String[] args) {
+        //prompts for input and checks to see if it is a valid input
+        //if not it askes for a new input
+        System.out.print("Please enter a number between 1-9 - "); //Prompts for input
+        Scanner scan = new Scanner (System.in);
+        while (!scan.hasNextInt()){
+            System.out.println("Did not enter an int please enter and integer");
+            scan = new Scanner (System.in);
+        }
+        int input = scan.nextInt();             //Saves input
+        
+        //code for for loops
+        
+        // 'a' is a variable to count the numebr of lines
+        // 'b' is a variable to count the number of numbers on a line
+        // 'c' is a variable to increment the dashes
+        // 'd' is a variable to increment the spacing
+        System.out.println("Using for loops");
+        for (int stack = 0;stack<=3;stack++){
+            for(int a=0;a<=stack;a++){              //increments the line
+                for(int d=0;d<=(4-stack);d++){      //increments the spacing for the stacks
+                    System.out.print(" ");
+                }
+                for(int b=0;(b<=(stack*2));b++){    //prints out the number
+                    System.out.print((input+stack));
+                }
+            System.out.println();
+            }
+        for(int d=0;d<=(4-stack);d++){          //correct spacing for the dashes
+                    System.out.print(" ");
+        }           
+        for(int c=0;c<(1+stack*2);c++){         //prints out the dashes
+            System.out.print("-");
+        }
+        System.out.println();                   //indents line after the spacer dashes
+        }
+        
+        //code for while loops
+        System.out.println("Using while loops");
+        //resets all of my counter vairables
+        int stack=0;    //increments stacks
+        int a=0;        //increments the lines of numbers
+        int b=0;        //increments the number of numbers on a line
+        int c=0;        //increments the dashes
+        int d=0;        //increments the spacing
+        //this while loop is for the series of numbers, and it is incremented by stacks
+        while(stack <=3){   
+            a=0;        //resets variables
+            c=0;        //resets variables
+            while(a<=stack){
+                b=0;        //resets variables
+                d=0;        //resets variables
+                while(d<=(4-stack)){        
+                    System.out.print(" ");      //spaces the stacks
+                    d++;
+                }
+                while(b<=(stack*2)){            //Determines the number for each stack
+                    System.out.print((input+stack));
+                    b++;
+                }
+            System.out.println();               
+            a++;                                //increments the line by 1
+            }
+        d=0;
+        while(d<=(4-stack)){
+            System.out.print(" ");              //spaces out the dashes
+            d++;
+        }
+        while(c<(1+stack*2)){
+            System.out.print("-");              //inserts spacer dashes
+            c++;
+        }
+        System.out.println();                   //creates a new line
+        stack++;                                //increments the stack
+            
+            
+        }
+        
+        
+       //do-while loops
+       System.out.println("Using do-while loops");
+       //resets all of my counter variables
+         stack=0;    //increments stacks
+        a=0;        //increments the lines of numbers
+        b=0;        //increments the number of numbers on a line
+        c=0;        //increments the dashes
+        d=0;        //increments the spacing
+       
+       do{
+           a=0;             //resets variables
+           c=0;             //resets variables
+           do{
+                b=0;        //resets variables
+                d=0;        //resets variables
+                do{
+                   System.out.print(" ");       
+                   d++;
+                }while(d<=(4-stack));           //incremetns spacing for stacks
+                do{
+                   System.out.print((input+stack));     //prints out stack numbers
+                   b++;
+                }while(b<=(stack*2));                   //increments the number of numbers on a line
+                System.out.println();
+                a++;
+            }while(a<=stack);                           //increments number of lines
+            d=0;
+            do{
+                System.out.print(" ");                  //spaces out the dashes
+                d++;
+            }while(d<=4-stack);
+            do{
+                System.out.print("-");                  //prints out the dashes
+                c++;
+            }while(c<(1+stack*2));
+            System.out.println();                       //indents to new line
+            stack++;                                    //increments stacks
+        }while(stack <=3);
+    }
+}
+    
